19ab543117e6e3e0721124d993357cb5354e5c96
jkunow <j.kunow@tu-berlin.de>
2022-05-18 20:02:50 +0200

    add test case

--- RsaTest.py
+++ RsaTest.py
@@ -5,12 +5,16 @@ import rsa_startK1 as rsa
 
 class MyTestCase(unittest.TestCase):
     def testPGCD(self):
-        print('fick dich')
-        self.assertEqual(1, rsa.pgcd(7,8))  # add assertion here
+        self.assertEqual(1, rsa.pgcd(7, 8))  # add assertion here
 
     def testCoPrime(self):
         self.assertEqual(True, rsa.isCoprime(3, 18980))
         self.assertEqual(False, rsa.isCoprime(9, 81))
+
+    def testExpoModRapide(self):
+        self.assertEqual(650, rsa.expo_modulaire_rapide(4334432, 543245, 4345))
+        self.assertEqual(1728, rsa.expo_modulaire_rapide(3, 12, 4567876543456789))
+
 
 
 
