37de40d9ae447d51dcb257baeaeec72bf4bad2c9
Laszlo Joel <cavalar@campus.tu-berlin.de>
2022-05-18 20:06:43 +0200

    add tests

--- RsaTest.py
+++ RsaTest.py
@@ -12,14 +12,19 @@ class MyTestCase(unittest.TestCase):
     def testCoPrime(self):
         self.assertEqual(True, rsa.isCoprime(3, 18980))
         self.assertEqual(False, rsa.isCoprime(9, 81))
-
-
-
-
         self.assertEqual(True, rsa.isCoprime(3, 18980))
 
     def testEuclideExt(self):
+        self.assertEqual((1,37,-3), rsa.euclide_ext(13,160))
         self.assertEqual((1,27,-19987), rsa.euclide_ext(57000,77))
+        self.assertEqual((3,0,1), rsa.euclide_ext(6,3))
+        self.assertEqual((3,0,1), rsa.euclide_ext(0,3))
+        self.assertEqual((4,1,0), rsa.euclide_ext(4,0))
+
+    def testInverseMod(self):
+        #self.assertEqual(1, rsa.inverse_modulaire(7,8))
+        #self.assertEqual(2, rsa.pgcd(4,2))
+    
 
 
 
