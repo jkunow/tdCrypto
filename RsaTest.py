4f2108061aebd83859c4ec09a71599a8da5e413f
jkunow <j.kunow@tu-berlin.de>
2022-05-18 19:50:03 +0200

    Clean tests

--- RsaTest.py
+++ RsaTest.py
@@ -1,15 +1,16 @@
 import unittest
 
-import rsa_startK1
+import rsa_startK1 as rsa
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
-
     def testCoPrime(self):
+        self.assertEqual(True, rsa.isCoprime(3, 18980))
+        self.assertEqual(False, rsa.isCoprime(9, 81))
 
-        self.assertEqual(True, rsa_startK1.isCoprime(3, 18980))
+
+
+
 
 
 if __name__ == '__main__':
